e5db7ae7c3bcf2a5370c4e5cbae9a214f00cd7f9
omar304050 <omaralalfy30@gmail.com>
2025-07-28 04:16:41 +0300

    hello world

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Header from '../components/Header'
-import SpecilatyMenu from '../components/specilatyMenu'
+import SpecialtyMenu from '../components/SpecialtyMenu'
 import TopDoctors from '../components/TopDoctors'
 import Banner from '../components/Banner'
 
@@ -8,7 +8,7 @@ const Home = () => {
   return (
     <div>
       <Header />
-     <SpecilatyMenu />
+     <SpecialtyMenu />
      <TopDoctors />
      <Banner />
       
